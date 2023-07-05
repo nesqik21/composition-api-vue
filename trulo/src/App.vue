d918a35c27c6334b9a8738dc98928a333a87da95
nesqik21 <tomas.pudis@gmail.com>
2023-07-05 18:20:14 +0200

    ref and computed

---
+++ trulo/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style>
+nav {
+  display: none;
+}
+</style>
---
+++ trulo/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="score++" class="btn btn-green">{{ score }}</button>
+    <p>Twice the score is: {{ double }}</p>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+export default {
+  setup() {
+    const score = ref(0);
+    const double = computed(() => score.value * 2);
+    return { score, double };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
