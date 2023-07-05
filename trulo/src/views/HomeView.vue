148e9bec022a4f22a18f984dc78c49a555afb6a1
nesqik21 <tomas.pudis@gmail.com>
2023-07-05 18:30:55 +0200

    reactive, torefs, computed, onmounted

--- trulo/src/views/HomeView.vue
+++ trulo/src/views/HomeView.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <button @click="score++" class="btn btn-green">{{ score }}</button>
-    <p>Twice the score is: {{ double }}</p>
+    <p>Twice the score is: {{ doubleTheScore }}</p>
     <br />
     <p>My name is: {{ myNameIs }}</p>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from "vue";
+import { reactive, toRefs, computed, onMounted } from "vue";
 export default {
   setup() {
     const state = reactive({
       score: 0,
       myNameIs: "Tomas",
+      doubleTheScore: computed(() => state.score * 2),
     });
+    onMounted(() => alert("I AM READY"));
     return { ...toRefs(state) };
   },
 };
