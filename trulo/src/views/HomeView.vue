14e13b1cfd0eac4b13543ce803889b5b235639e7
nesqik21 <tomas.pudis@gmail.com>
2023-07-05 18:23:00 +0200

    reactive, toRefs

--- trulo/src/views/HomeView.vue
+++ trulo/src/views/HomeView.vue
@@ -2,16 +2,20 @@
   <div>
     <button @click="score++" class="btn btn-green">{{ score }}</button>
     <p>Twice the score is: {{ double }}</p>
+    <br />
+    <p>My name is: {{ myNameIs }}</p>
   </div>
 </template>
 
 <script>
-import { ref, computed } from "vue";
+import { reactive, toRefs } from "vue";
 export default {
   setup() {
-    const score = ref(0);
-    const double = computed(() => score.value * 2);
-    return { score, double };
+    const state = reactive({
+      score: 0,
+      myNameIs: "Tomas",
+    });
+    return { ...toRefs(state) };
   },
 };
 </script>
